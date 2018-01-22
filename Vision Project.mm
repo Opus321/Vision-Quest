7e9196420786ddef3afbd3fdb5c86aa48424c588
Opus321 <34922349+Opus321@users.noreply.github.com>
2018-01-22 15:02:44 -0800

    Add files via upload

---
+++ Vision Project.mm
@@ -0,0 +1,111 @@
+<map version="freeplane 1.5.9">
+<!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
+<node TEXT="Vision&#xa;Project" FOLDED="false" ID="ID_494350403" CREATED="1514226846443" MODIFIED="1514227564461" STYLE="oval">
+<font SIZE="18" ITALIC="true"/>
+<hook NAME="MapStyle">
+    <properties fit_to_viewport="false;"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24.0 pt">
+<font SIZE="24"/>
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="default" COLOR="#000000" STYLE="fork">
+<font NAME="SansSerif" SIZE="10" BOLD="false" ITALIC="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.attributes">
+<font SIZE="9"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.note" COLOR="#000000" BACKGROUND_COLOR="#ffffff" TEXT_ALIGN="LEFT"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#cc3300" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#669900">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important">
+<icon BUILTIN="yes"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000" STYLE="oval" SHAPE_HORIZONTAL_MARGIN="10.0 pt" SHAPE_VERTICAL_MARGIN="10.0 pt">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#0033ff">
+<font SIZE="16"/>
+<edge COLOR="#ff0000"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#00b439">
+<font SIZE="14"/>
+<edge COLOR="#0000ff"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#990000">
+<font SIZE="12"/>
+<edge COLOR="#00ff00"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#111111">
+<font SIZE="10"/>
+<edge COLOR="#ff00ff"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,5">
+<edge COLOR="#00ffff"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,6">
+<edge COLOR="#7c0000"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,7">
+<edge COLOR="#00007c"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,8">
+<edge COLOR="#007c00"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,9">
+<edge COLOR="#7c007c"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,10">
+<edge COLOR="#007c7c"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,11">
+<edge COLOR="#7c7c00"/>
+</stylenode>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<hook NAME="AutomaticEdgeColor" COUNTER="3" RULE="ON_BRANCH_CREATION"/>
+<node TEXT="Hardware" POSITION="right" ID="ID_1366053434" CREATED="1514226906348" MODIFIED="1514226915442">
+<edge COLOR="#ff0000"/>
+<node TEXT="MS Kinect Camera" ID="ID_637188437" CREATED="1514226940174" MODIFIED="1514226978452"/>
+<node TEXT="Raspberry PI v2" ID="ID_1432350795" CREATED="1514226982443" MODIFIED="1514226997426"/>
+<node TEXT="MS PC" ID="ID_1595699948" CREATED="1514227005191" MODIFIED="1514227044589"/>
+</node>
+<node TEXT="Software" POSITION="left" ID="ID_1490330400" CREATED="1514226926728" MODIFIED="1514226933980">
+<edge COLOR="#0000ff"/>
+<node TEXT="Oracle VirtualBox SW" ID="ID_1635122375" CREATED="1514227051226" MODIFIED="1514227070908">
+<node TEXT="VirtualBox.org" ID="ID_862604675" CREATED="1514227155838" MODIFIED="1514227256009" LINK="https://www.virtualbox.org/"/>
+</node>
+<node TEXT="Ubuntu Linux" ID="ID_1848191437" CREATED="1514227090231" MODIFIED="1514227288748">
+<node TEXT="YouTube" ID="ID_1308626649" CREATED="1514227310556" MODIFIED="1514227326144" LINK="https://www.youtube.com/watch?v=CkDd6jClqEE"/>
+</node>
+<node TEXT="TurtleBot Simulator" ID="ID_461162489" CREATED="1514227503462" MODIFIED="1514227512793">
+<node TEXT="Overview Installation" ID="ID_324752902" CREATED="1514227517256" MODIFIED="1514227531703" LINK="https://www.youtube.com/watch?v=9U6GDonGFHw"/>
+</node>
+</node>
+<node TEXT="Project Outline" POSITION="right" ID="ID_288958904" CREATED="1514227687324" MODIFIED="1514227986035">
+<edge COLOR="#00ff00"/>
+<node TEXT="Proposal" ID="ID_231416997" CREATED="1514227990676" MODIFIED="1514227996587"/>
+<node TEXT="Time Line" ID="ID_1504187182" CREATED="1514227998632" MODIFIED="1514228209388"/>
+<node TEXT="Requirements" ID="ID_1967371886" CREATED="1514228012892" MODIFIED="1514228184666"/>
+<node TEXT="Deliverables" ID="ID_478614455" CREATED="1514228187043" MODIFIED="1514228200164"/>
+</node>
+</node>
+</map>
